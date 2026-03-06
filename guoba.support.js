38bd2cd14c477202edfd79f3b959261d80ce47fa
ethan42nd <buhuo4805@gmail.com>
2026-03-07 04:39:25 +0800

    fix: 锅巴配置接口池

--- guoba.support.js
+++ guoba.support.js
@@ -2157,18 +2157,13 @@ export function supportGuoba() {
         },
         {
           component: "Divider",
-          label: "智能接口池说明",
-          componentProps: { orientation: "left", plain: true },
-        },
-        {
-          component: "Divider",
           label: "智能接口池",
           componentProps: { orientation: "left", plain: true },
         },
         {
           field: "smart_APIList",
           label: "接口列表",
-          bottomHelpMessage: "在这里新增或管理你的 AI 接口。⚠️ 修改并【保存】后，请【刷新当前网页】，即可在下方的下拉菜单中选用新接口。",
+          bottomHelpMessage: "在这里新增或管理你的 AI 接口。⚠️ 新增并【保存】后，请【刷新当前网页】，即可在下方的下拉菜单中选用新接口。",
           component: "GSubForm",
           componentProps: {
             multiple: true,
@@ -2176,7 +2171,6 @@ export function supportGuoba() {
               {
                 field: "remark",
                 label: "标识名(必填)",
-                bottomHelpMessage: "作为这个接口的唯一名字，例如：备用-Gemini",
                 component: "Input"
               },
               {
@@ -2198,13 +2192,11 @@ export function supportGuoba() {
               {
                 field: "apiKey",
                 label: "API密钥",
-                bottomHelpMessage: "填入密钥。如果留空，将自动抽取全局配置的 sf_keys",
                 component: "InputPassword"
               },
               {
                 field: "modelId",
                 label: "模型名称",
-                bottomHelpMessage: "例如：Qwen/Qwen2.5-7B-Instruct",
                 component: "Input"
               }
             ]
